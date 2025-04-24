6e6b920be416d2ab94d1b0ab2f525f1703bf50c0
Gustavo Szigethi <61527120+Guetti@users.noreply.github.com>
2025-04-24 05:31:25 -0400

    feat: configurar base de datos MySQL y añadir archivo .env
    Se ha actualizado la configuración de la base de datos en settings.py para utilizar MySQL en lugar de SQLite. Además, se ha añadido un archivo .env.example para gestionar las variables de entorno necesarias para la conexión a la base de datos.

--- backend/todo_project/settings.py
+++ backend/todo_project/settings.py
@@ -10,10 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
 from pathlib import Path
+
+import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -82,8 +89,15 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
